3025cc21f5b66b50b04c26a1e36cdfb82c20f77c
HakinRuslan <flowstandoff19@gmail.com>
2025-02-21 22:51:28 +0500

    123

--- consumer.py
+++ consumer.py
@@ -8,19 +8,29 @@ import uuid
 import json
 
 consumer = KafkaConsumer(bootstrap_servers='5.35.98.86:9092', group_id="kafka_1")
-consumer.subscribe(["ruslankrutou"])
 
-try:
-    while True:
-        msg = consumer.poll(timeout=1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            raise KafkaException(msg.error())
-        else:
-            # действия с полученным сообщением
-            logging.info(f"Received message: {msg.value().decode('utf-8')}")
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
+def cons_loop(consumer, topics):
+    try:
+        consumer.subscribe(["ruslankrutou"])
+
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                raise KafkaException(msg.error())
+            else:
+                # действия с полученным сообщением 
+                data = json.loads(msg.value().decode('utf-8'))
+                logging.info(f"Received message: {data}")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
+    
+def main():
+    cons_loop(consumer)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    main()
--- producer.py
+++ producer.py
@@ -10,22 +10,30 @@ from kafka import KafkaProducer
 
 from faker import Faker
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 logger = logging.getLogger(__name__)
 
 fak = Faker("ru_RU")
 
 producer = KafkaProducer(bootstrap_servers='5.35.98.86:9092')
 
-while True:
-    fullname = fak.name()
-    fullnames = [str(name) for name in fullname.split(' ')]
-    logging.info(fullnames)
-    user = {"id": str(uuid.uuid4()), "firstname": fullnames[0], "Lastname": fullnames[1], "Secondname": fullnames[2], "addres": fak.street_address(), "index": fak.postcode() , "number": fak.phone_number(), "work": fak.job()}
-    json_data = json.dumps(user)
-    byte_data = json_data.encode("utf-8")
-    logging.info(user)
-    future = producer.send('ruslankrutou', byte_data)
-    result = future.get()
-    logging.info(result)
+
+def producer_send(producer, topics):
+    while True:
+        fullname = fak.name()
+        fullnames = [str(name) for name in fullname.split(' ')]
+        logging.info(fullnames)
+        user = {"id": str(uuid.uuid4()), "firstname": fullnames[0], "Lastname": fullnames[1], "Secondname": fullnames[2], "addres": fak.street_address(), "index": fak.postcode() , "number": fak.phone_number(), "work": fak.job()}
+        json_data = json.dumps(user)
+        byte_data = json_data.encode("utf-8")
+        logging.info(user)
+        future = producer.send(topics, byte_data)
+        result = future.get()
+        logging.info(result)
+
+
+def main():
+    producer_send(producer, "ruslankrutou")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    main()
